152f6493a6b5ca341f6065c6124cdb3a1d7b7dd6
DAM1 <jade>
2024-11-04 11:43:13 +0100

    Merge branch 'main' of https://github.com/jadelpz/eje2.git into main

---
+++ 1DAM/PROGRAMACION/WORDSPACE/Eje2/src/retos/eje2.java
@@ -0,0 +1,15 @@
+package retos;
+
+public class eje2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+for (int i = 0; i < 10; i++) {
+	
+	System.out.println("el valor" +i);
+	System.out.println("el numero es"+i*3);
+	
+}
+	}
+
+}
--- 1DAM/PROGRAMACION/WORDSPACE/reto0/src/reto0/reto0.java
+++ 1DAM/PROGRAMACION/WORDSPACE/reto0/src/reto0/reto0.java
@@ -2,9 +2,24 @@ package reto0;
 
 public class reto0 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-System.out.println("hola mundo");
+	public static int sumar (int a , int b) {
+		return a+b; 
+		}
+	
+	public static int restar (int a , int b) {
+		
+		return a-b; 
+		}
+	public static void ParImpar (int numero) {
+	
+	if (numero % 2 == 0) {
+		
+		System.out.println(numero + " par.");
+	
+	} else {
+		
+		System.out.println(numero + " impar.");
+	}
 	}
 
 }
